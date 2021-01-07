286cb10dc05cd9d6f7c2dd84fe354c20c6b0f800
Oleksandr Ryzhkov <ryzhman@gmail.com>
2021-01-07 09:29:38 -0500

    Custom-built pipe to transform the data in the model

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import {Component, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
+import {AppComponent} from './app.component';
+import {MyFirstComponentComponent} from './my-first-component/my-first-component.component';
 import {WelcomeComponent} from "./home/welcome.component";
-import { ProductListComponent } from './product-list-component/product-list.component';
+import {ProductListComponent} from './product-list-component/product-list.component';
 import {FormsModule} from "@angular/forms";
+import {ConvertToSpacePipe} from "../pipes/ConvertToSpacePipe";
 
 @NgModule({
-  // all the components should be included here
+  // all the custom components should be included here
   declarations: [
     AppComponent,
     MyFirstComponentComponent,
     WelcomeComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacePipe
   ],
+  // out of box components
   imports: [
     BrowserModule,
     FormsModule
@@ -22,4 +25,5 @@ import {FormsModule} from "@angular/forms";
   // Only parent component must be added here
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
